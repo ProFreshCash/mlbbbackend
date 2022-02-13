f6c2abbfea1abe7ae55ff0a014f812d963b2b460
ProFreshCash <11a.levaitibor@gmail.com>
2022-02-13 16:58:55 +0100

    teszt

--- backend.js
+++ backend.js
@@ -85,6 +85,33 @@ app.get('/tankok', (req, res) => {
 
   
   })
+
+  app.get('/osszestargy', (req, res) => {
+
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'mlbb'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT `item_nev`,`ar`,items.leiras,items.kep,items_type.item_fajta FROM `items` INNER JOIN items_type ON items_type.id=items.tipus_id ', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log(rows)
+      res.send(rows)
+    })
+    
+    connection.end()
+  
+  
+  
+  })
+
+
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
